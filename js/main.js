985ff5963f6e2543ac932f3580f40801ab90db46
milicev <boyan93@gmail.com>
2018-05-22 19:29:44 +0200

    add css 70%

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// results slider
+$(document).ready(function(){
+  $('.results-slider').slick({
+    inifite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 300,
+    arrows: false
+  });
+});
