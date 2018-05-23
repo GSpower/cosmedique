88b438dce6d7a370c321f43c42e76235ea5ef546
milicev <boyan93@gmail.com>
2018-05-23 19:58:28 +0200

    add tablet version

--- js/main.js
+++ js/main.js
@@ -6,6 +6,24 @@ $(document).ready(function(){
     slidesToScroll: 1,
     autoplay: true,
     speed: 300,
-    arrows: false
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+      }
+      }
+    ]
   });
+});
+// burger menu
+$(document).ready(function(){
+  var burgerMenu = $('.header-top__nav');
+  var burgerMenuBtn = $('.responsive-menu__btn');
+  
+  burgerMenuBtn.on('click', function () {
+    burgerMenu.slideToggle('header-top__nav-list--active');
+  })
+
 });
