7c0dbb23c815321d8aad1e7340963fd4088861e8
milicev <boyan93@gmail.com>
2018-05-30 19:57:16 +0200

    add functionality to popup

--- js/main.js
+++ js/main.js
@@ -25,11 +25,59 @@ $(document).ready(function(){
     ]
   });
 });
-// burger menu
 $(document).ready(function(){
+  // burger menu
   var burgerMenu = $('.header-top__nav');
   var burgerMenuBtn = $('.responsive-menu__btn');
   burgerMenuBtn.on('click', function () {
     burgerMenu.slideToggle('header-top__nav-list--active');
   })
+  // special offer opening form
+  var btnForOpeningOfferingPopup = $('.special-offer-open');
+  var btnForClosingOfferingPopup = $('.special-offer-popup__close-btn');
+  var specialOfferPopup = $('.special-offer-popup__wrap');
+
+  btnForOpeningOfferingPopup.on('click', function () {
+    specialOfferPopup.show();
+  })
+  btnForClosingOfferingPopup.on('click', function () {
+    specialOfferPopup.hide();
+  })
+  // special offer form
+  var offeringPopupEmail = $('#popup-email-address');
+	var offeringPopupPhone = $('#popup-phone-number');
+	var offeringPopupBtn = $('#popup-btn');
+	var offeringHolder = $('#offering-form');
+	var offeringMsg = $('.offering-form__msg');
+
+	offeringPopupBtn.on('click', function (e) {
+		e.preventDefault();
+		var formEmail = offeringPopupEmail.val();
+		var formPhoneNumb = offeringPopupPhone.val();
+		
+		if(formEmail.length > 0 || formPhoneNumb > 0){
+			$.post('https://offers.qyksonic.com/test/offer.php', { email: formEmail, phone: formPhoneNumb }, function(response) {
+				if (response.success == 1) {
+					// console.log("Success!");
+					window.location.href='https://qyksonic.com/products/zoe%E2%84%A2-special-offer';
+					
+				} else if (response.error < 0) {
+					// console.log("Failed to send data");
+					offeringMsg.addClass('offering-form__msg--bl')
+					offeringMsg.text("Invalid e-mail address, please try again.")
+				}
+			});
+		}else{
+			// console.log("fill one of inputs");
+			offeringMsg.addClass('offering-form__msg--bl');
+			offeringMsg.text("Please enter your e-mail address or phone number.")
+		}
+		
+  })
+  // click outside
+  $(document).click(function(event) {
+    if (!$(event.target).closest(".special-offer-popup, .special-offer-open").length) {
+      $("body").find(".special-offer-popup__wrap").hide() ;
+    }
+  });
 });
